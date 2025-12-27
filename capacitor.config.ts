c686a5e1bc81d820c08cd8d1431b54feb91de057
fthern <fatiherenguzel@gmail.com>
2025-12-27 04:58:45 +0100

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.youware.germanverbs',
+  appName: 'German Verbs',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
